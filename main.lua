f58d8856bbf0933866e04fc0d81e043c7498720d
cl0wn_wormz <82059166+cl0wnZwormz@users.noreply.github.com>
2024-06-05 00:29:11 +0300

    everything

---
+++ main.lua
@@ -0,0 +1,22 @@
+local img = get("boobimg")
+local button = get("newbutton")
+
+function generateImage()
+    local res = fetch({
+        url = "https://nekobot.xyz/api/image?type=boobs",
+        method = "GET",
+        headers = { ["Content-Type"] = "application/json" },
+        body = ""
+    })
+
+    local toresize = res["message"]
+    print(toresize)
+    local urll = "http://tr.operium.org:1112/resizeimg/?imageUrl=" .. toresize .. "&width=500&height=500&quality=50"
+    img.set_source(urll)
+end
+
+generateImage()
+
+button.on_click(function ()
+    generateImage()
+end)
